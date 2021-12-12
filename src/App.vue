9f28f0ee2efffa842c7c85abce5288cae028ffcb
ENIO1910 <patrykstaniewski1910@gmail.com>
2021-12-12 20:53:49 +0100

    Finish v1.0

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,10 @@
     <Claim v-if="step === 0"/>
     <SearchInput v-model="searchValue" @input="handleInput" :dark="step === 1" />
     <div class="results" v-if="results && !loading && step === 1">
-      <Item v-for="item in results" :item="item" :key="item.data[0].nasa_id"/>
+      <Item v-for="item in results" :item="item" :key="item.data[0].nasa_id" @click.native="handleModalOpen(item)"/>
     </div>
+    <div class="loader" v-if="step=== 1 && loading"><div></div><div></div><div></div><div></div></div>
+    <Modal v-if="modalOpen" :item="modalItem" @closeModal="modalOpen = false"/>
   </div>
 </template>
 
@@ -27,8 +29,11 @@ import HeroImage from '@/components/HeroImage.vue';
 
 import Item from '@/components/Item.vue';
 
+import Modal from '@/components/Modal.vue';
+
 const API = 'https://images-api.nasa.gov/search';
 export default {
+  /* eslint max-len: ["error", { "code": 800 }] */
   /* eslint-disable */
   name: 'App',
   components: {
@@ -36,11 +41,14 @@ export default {
     SearchInput,
     HeroImage,
     Item,
+    Modal,
   },
   
 
   data() {
     return {
+      modalOpen: false,
+      modalItem: null,
       loading:false,
       step: 0,
       searchValue: '',
@@ -48,6 +56,10 @@ export default {
     };
   },
   methods: {
+    handleModalOpen(item) {
+      this.modalOpen = true;
+      this.modalItem = item;
+    },
     // eslint-disable-next-line
     handleInput: debounce(function() {
       this.loading = true;
@@ -136,4 +148,57 @@ export default {
     }
 
   }
+  .results:hover{
+    cursor:pointer;
+  }
+
+  .loader {
+  margin-top:100px;
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+
+    @media (min-width: 768px) {
+      width: 120px;
+      height: 120px;
+      
+    }
+  }
+  .loader div {
+  box-sizing: border-box;
+  display: block;
+  position: absolute;
+  width: 64px;
+  height: 64px;
+  margin: 8px;
+  border: 8px solid rgb(250, 240, 220);
+  border-radius: 50%;
+  animation: loading 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+  border-color: rgb(250, 240, 220) transparent transparent transparent;
+
+  @media (min-width: 768px) {
+      width: 120px;
+      height: 120px;
+      
+    }
+
+}
+.loader div:nth-child(1) {
+  animation-delay: -0.45s;
+}
+.loader div:nth-child(2) {
+  animation-delay: -0.3s;
+}
+.loader div:nth-child(3) {
+  animation-delay: -0.15s;
+}
+@keyframes loading {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
 </style>
